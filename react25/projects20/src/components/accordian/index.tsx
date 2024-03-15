ac125ed1dfe0fe4ce359cfd82bbac3c6cca46050
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-03-15 11:23:21 +0530

    feat: accordion

--- react25/projects20/src/components/accordian/index.tsx
+++ react25/projects20/src/components/accordian/index.tsx
@@ -1,27 +1,55 @@
 import React, { useState } from 'react';
 import data from './data';
+import './style.css';
 
 const Accordion: React.FC = () => {
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState<string | null>();
+  const [enableMultiSelection, setEnableMultiSelection] =
+    useState<boolean>(false);
+  const [multiple, setMultiple] = useState<string[]>([]);
 
-  const handleSingleSelect = (getCurrentId) => {
-    console.log(getCurrentId);
+  const handleSingleSelect = (getCurrentId: string) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
   };
+
+  const handleMultiSelection = (getCurrentId: string) => {
+    const copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+    if (findIndexOfCurrentId === -1) {
+      copyMultiple.push(getCurrentId);
+    } else {
+      copyMultiple.splice(findIndexOfCurrentId, 1);
+    }
+    setMultiple(copyMultiple);
+  };
+  console.log(selected, multiple);
 
   return (
     <>
       <div className="wrapper">
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+          {' '}
+          Enable multiple selection{' '}
+        </button>
         <div className="accordion">
           {data && data.length > 0 ? (
             data.map((dataItem) => (
               <div className="item">
                 <div
-                  onClick={() => handleSingleSelect(dataItem.id)}
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelection(dataItem.id)
+                      : () => handleSingleSelect(dataItem.id)
+                  }
                   className="title"
                 >
                   <h3> {dataItem.question}</h3>
                   <span> + </span>
                 </div>
+                {selected === dataItem.id ||
+                multiple.indexOf(dataItem.id) !== -1 ? (
+                  <div className="content"> {dataItem.answer} </div>
+                ) : null}
               </div>
             ))
           ) : (

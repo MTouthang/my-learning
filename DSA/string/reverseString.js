f4ae5a6d6cabeecf3da5c357ab1f3dfc2c3b9b93
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-07-02 19:43:34 +0530

    feat:  brute reverse string

--- DSA/string/reverseString.js
+++ DSA/string/reverseString.js
@@ -3,3 +3,9 @@ const str = "hello world";
 // inbuilt method --
 const result = str.split("").reverse().join("");
 console.log(result);
+
+let revStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  revStr += str[i];
+}
+console.log(revStr);
